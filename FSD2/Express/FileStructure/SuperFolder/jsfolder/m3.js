9fa13ead45a3af9d82aae9359ee7d66a481f3112
Moksh Kothari <165584098+Moksh2125@users.noreply.github.com>
2025-04-30 10:44:13 +0530

    Delete FSD2/Express directory

--- FSD2/Express/FileStructure/SuperFolder/jsfolder/m3.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const app = express();
-const path = require('path')
-const sp = path.join(__dirname,'../');
-app.use(express.static(sp,{index : 'm3.html'}));
-app.listen(8001)
